eff05f964e716dd6833aee1db71b4ac6e85112f5
tvpluss <truongphuocst310301@gmail.com>
2021-10-18 19:05:40 +0700

    added style for sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,16 +7,18 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ProfileButton from "../ProfileButton/ProfileButton";
+// import { Link } from "react-router-dom";
 
 
 export default function Sidebar(){
     return(
         <div className="sidebar">
-        <Logo></Logo>
-        <SidebarOption active Icon={BusinessCenterIcon} text="Tìm việc"></SidebarOption>
+        <Logo className="Viecmoi-logo"></Logo>
+        <SidebarOption active Icon={BusinessCenterIcon} text="Tìm việc" ></SidebarOption>
         <SidebarOption Icon={FormatListBulletedIcon} text="Việc của tôi"></SidebarOption>
         <SidebarOption Icon={ChatIcon} text="Tin nhắn"></SidebarOption>
         <SidebarOption Icon={NotificationsIcon} text="Thông báo"></SidebarOption>
+        <div className="padding"></div>
         <ProfileButton></ProfileButton>
         </div>
     )
