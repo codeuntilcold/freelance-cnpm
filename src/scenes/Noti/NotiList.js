cadafcafe6c0294ca8f4167f363631f42d7b1696
QuocDung <quocdungngole@gmail.com>
2021-10-24 03:18:40 +0700

    thêm Noti

---
+++ src/scenes/Noti/NotiList.js
@@ -0,0 +1,22 @@
+import NotiItem from "./NotiItem"
+
+function NotiList() {
+
+    return (
+        <div className='noti-list'>
+
+            <div className="noti-header">
+                <h1>Thông báo</h1>
+            </div>
+
+            <NotiItem />
+            <NotiItem />
+            <NotiItem />
+            <NotiItem />
+            <NotiItem />
+            <NotiItem />
+        </div>
+    )
+}
+
+export default NotiList
