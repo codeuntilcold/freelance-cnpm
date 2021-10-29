61aca8cd40b02d05e3d5848fe64140204f281cbc
QuocDung <quocdungngole@gmail.com>
2021-10-29 15:53:10 +0700

    hoàn thiện Noti

--- src/scenes/Noti/NotiItem.js
+++ src/scenes/Noti/NotiItem.js
@@ -3,7 +3,9 @@ function NotiItem({ content }) {
     return (
         <div className='noti-item'>
             {/* {content} */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, doloribus?
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, doloribus?
+            </p>
         </div>
     )
 }
