e2583568856fcf5946e5aeba81d16f0e0e7aad97
QuocDung <quocdungngole@gmail.com>
2021-10-30 17:03:31 +0700

    thêm các class để đồng bộ

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,10 @@
+export default function PageHeader({ title }) {
+
+    return (
+        <div class='central-page-header'>
+            <h1>
+                {title}
+            </h1>
+        </div>
+    )
+}
