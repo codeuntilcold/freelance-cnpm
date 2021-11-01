d32162f599884c8bad4a41adbd1a6bb8b8f2adbf
Khoa <khoa20082001@gmail.com>
2021-11-01 23:25:50 +0700

    make homepage responsive + tweak layout

--- src/scenes/Home/components/Footer/Footer.js
+++ src/scenes/Home/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Logo from '../../../../components/Logo/Logo'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Footer.css'
 
 const Footer = () => {
