bfcbd64b110a512120fc2e56ac40e28b6784eee4
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2021-11-03 23:25:47 +0700

    đồng bộ data job-management

--- src/scenes/JobManagement/Component/FreelancerManagement/AcceptedItem.js
+++ src/scenes/JobManagement/Component/FreelancerManagement/AcceptedItem.js
@@ -6,8 +6,8 @@ function AcceptedItem(props){
     return (
         <div className = "item-container item-container--accepted">
             <p className = "item__name">{props.name}</p>
-            <p className = "item__field item-content">{props.job}</p>
-            <p className= "item__field item__field--name">{props.info}</p>
+            <p className = "item__field item-content">{props.addresss}</p>
+            <p className= "item__field item__field--name">{props.aboutMe}</p>
             <p className = "item__button">
                 <AccessButton
                     name = "Chi tiết"
--- src/scenes/JobManagement/Component/data/freelancer.js
+++ src/scenes/JobManagement/Component/data/freelancer.js
@@ -1,40 +1,101 @@
 let freelancerList = [
     {
-        "id": 1,
-        "jodId": 1,
-        "name": "Nguyễn Văn An",
-        "age": 30,
-        "job": "Lập trình viên",
-        "detail": "Có 5 năm kinh nghiệm trong lập trình web",
-        "accepted": false 
+        "name": "Nguyễn Văn Minh",
+        "address": [ "33/3 Ung Văn Khiêm, quận Bình Thạnh, TP.HCM" ],
+        "phone": "0945123456",
+        "email": "minhnguyen12@gmail.com",
+        "about-me": "Lao động tự do, cần tìm một công việc sau dịch bệnh.",
+        "desired-hourly-salary": "25000 VND",
+        "skills": ["bưng bê", "rửa chén", "phụ bếp", "thu ngân", "quét dọn"],
+        "education": ["cấp 3"],
+        "_id": "fr1",
+        "createdAt": "05/11/2021"
     },
     {
-        "id": 2,
-        "jodId": 1,
-        "name": "Nguyễn Văn Bình",
-        "age": 34,
-        "job": "Lập trình viên",
-        "detail": "Có 8 năm kinh nghiệm trong lập trình web",
-        "accepted": false 
+        "name": "Nguyễn Văn Toàn",
+        "address": [ "33/3 Ung Văn Khiêm, quận Bình Thạnh, TP.HCM" ],
+        "phone": "0945123456",
+        "email": "toannguyen12@gmail.com",
+        "about-me": "Lao động tự do, cần tìm một công việc sau dịch bệnh.",
+        "desired-hourly-salary": "25000 VND",
+        "skills": ["bưng bê", "rửa chén", "phụ bếp", "thu ngân", "quét dọn"],
+        "education": ["cấp 3"],
+        "_id": "fr2",
+        "createdAt": "05/11/2021"
     },
     {
-        "id": 3,
-        "jodId": 1,
-        "name": "Nguyễn Đức",
-        "age": 28,
-        "job": "Lập trình viên",
-        "detail": "Từng tham gia 12 dự án web",
-        "accepted": true 
+        "name": "Trần Văn Tân",
+        "address": [ "33/3 Ung Văn Khiêm, quận Bình Thạnh, TP.HCM" ],
+        "phone": "0945123456",
+        "email": "toannguyen12@gmail.com",
+        "about-me": "Lao động tự do, cần tìm một công việc sau dịch bệnh.",
+        "desired-hourly-salary": "25000 VND",
+        "skills": ["bưng bê", "rửa chén", "phụ bếp", "thu ngân", "quét dọn"],
+        "education": ["cấp 3"],
+        "_id": "fr3",
+        "createdAt": "05/11/2021"
     },
     {
-        "id": 4,
-        "jodId": 2,
-        "name": "Trần Công",
-        "age": 31,
-        "job": "Lập trình viên",
-        "detail": "Đã từng làm việc cho nhiều app mobile",
-        "accepted": false 
+        "name": "Nguyễn Văn Tường",
+        "address": [ "33/3 Ung Văn Khiêm, quận Bình Thạnh, TP.HCM" ],
+        "phone": "0945123456",
+        "email": "toannguyen12@gmail.com",
+        "about-me": "Lao động tự do, cần tìm một công việc sau dịch bệnh.",
+        "desired-hourly-salary": "25000 VND",
+        "skills": ["bưng bê", "rửa chén", "phụ bếp", "thu ngân", "quét dọn"],
+        "education": ["cấp 3"],
+        "_id": "fr4",
+        "createdAt": "05/11/2021"
+    },
+    {
+        "name": "Bùi Đức Anh",
+        "address": [ "33/3 Ung Văn Khiêm, quận Bình Thạnh, TP.HCM" ],
+        "phone": "0945123456",
+        "email": "toannguyen12@gmail.com",
+        "about-me": "Lao động tự do, cần tìm một công việc sau dịch bệnh.",
+        "desired-hourly-salary": "25000 VND",
+        "skills": ["bưng bê", "rửa chén", "phụ bếp", "thu ngân", "quét dọn"],
+        "education": ["cấp 3"],
+        "_id": "fr5",
+        "createdAt": "05/11/2021"
     }
+    // ,
+    // {
+    //     "id": 1,
+    //     "jodId": 1,
+    //     "name": "Nguyễn Văn An",
+    //     "age": 30,
+    //     "job": "Lập trình viên",
+    //     "detail": "Có 5 năm kinh nghiệm trong lập trình web",
+    //     "accepted": false 
+    // },
+    // {
+    //     "id": 2,
+    //     "jodId": 1,
+    //     "name": "Nguyễn Văn Bình",
+    //     "age": 34,
+    //     "job": "Lập trình viên",
+    //     "detail": "Có 8 năm kinh nghiệm trong lập trình web",
+    //     "accepted": false 
+    // },
+    // {
+    //     "id": 3,
+    //     "jodId": 1,
+    //     "name": "Nguyễn Đức",
+    //     "age": 28,
+    //     "job": "Lập trình viên",
+    //     "detail": "Từng tham gia 12 dự án web",
+    //     "accepted": true 
+    // },
+    // {
+    //     "id": 4,
+    //     "jodId": 2,
+    //     "name": "Trần Công",
+    //     "age": 31,
+    //     "job": "Lập trình viên",
+    //     "detail": "Đã từng làm việc cho nhiều app mobile",
+    //     "accepted": false 
+    // }
 
 ];
 
