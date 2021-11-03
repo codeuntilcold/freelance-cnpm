2c8614b322f6d221e6da8310ad15afb950141f14
QuocDung <quocdungngole@gmail.com>
2021-11-03 22:49:17 +0700

    preetier

--- src/scenes/Myjobs/index.js
+++ src/scenes/Myjobs/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Container from '@mui/material/Container';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
