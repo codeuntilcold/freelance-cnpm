2b93ed192ca56e10b98ba38c02ad5ea572037b73
QuocDung <quocdungngole@gmail.com>
2021-11-04 17:50:20 +0700

    hack để vào được job-management

--- src/components/CornerFooter/index.js
+++ src/components/CornerFooter/index.js
@@ -6,7 +6,7 @@ export default function CornerFooter() {
             <a href="#">Giới thiệu</a>
             <a href="#">Điều khoản dịch vụ</a>
             <a href="#">Chính sách quảng cáo</a>
-            <a href="#">&copy; 2021 Viecmoi.net</a>
+            <a href="/job-management">&copy; 2021 Viecmoi.net</a>
             <a href="#"></a>
         </div>
     )
