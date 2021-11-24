f385be47105707c833fa68250596e79c00eb1db7
QuocDung <quocdungngole@gmail.com>
2021-11-25 03:13:39 +0700

    [UI] Better logo on mobile

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,10 @@ import "./Logo.css"
 export default function Logo(){
     return (
         <div className="logo_container">
-            <Link to="/home" className="logo"> VIECMOI</Link>
+            <Link to="/home" className="logo">
+                <img src="/android-chrome-512x512.png" alt="V" width='35px' style={{ marginBottom: '12px' }}/>
+                <span>VIECMOI</span>
+            </Link>
         </div>
     )
 }
