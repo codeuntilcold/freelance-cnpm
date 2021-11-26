588c7787a40865e5353b07e52127bfab3143f9ee
Khoa <khoa20082001@gmail.com>
2021-11-26 22:59:00 +0700

    modify logo path

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Logo.css"
-
+import "./Logo.css";
+import auth from "../../services/auth";
+ 
 export default function Logo(){
+    var path;
+    if (auth.currentUser) {
+        path = '/jobsfeed'
+    }
+    else {
+        path = '/home'
+    }
+
     return (
         <div className="logo_container">
-            <Link to="/home" className="logo">
+            <Link to={path} className="logo">
                 <img src="/android-chrome-512x512.png" alt="V" width='35px' style={{ marginBottom: '12px' }}/>
                 <span>VIECMOI</span>
             </Link>
