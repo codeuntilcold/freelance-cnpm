3b7e18a95614d0b2122fa306711182c0180c239f
QuocDung <quocdungngole@gmail.com>
2022-02-09 12:38:03 +0700

    update simple server

---
+++ server/server.cjs
@@ -0,0 +1,17 @@
+// import path from 'path';
+// import express from 'express';
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
+
+app.listen(port, () => { 
+    console.log(`Server is running on port ${port}`);
+});
