166d4a1d43c8e2b8588ca6ec707cb2728b404905
QuocDung <quocdungngole@gmail.com>
2022-02-09 12:46:53 +0700

    update

--- server/server.cjs
+++ server/server.cjs
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
